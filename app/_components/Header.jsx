1bb17edf869f4e2d581f4bb91c948caa47c49737
mendsalbert <mendsalbert@gmail.com>
2024-07-13 08:16:21 +0000

    fix: correct the image source path in Header component
    The change shifts from a relative to an absolute path for the application's logo image in the Header component. This was prompted by issues with incorrect image loading in subdirectories of the app due to the use of a relative image source path. Now, with the absolute path, the logo should load consistently across all pages and levels of the application.

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
-        <Image src={"./chart-donut.svg"} alt="logo" width={40} height={25} />
+        <Image src={"/chart-donut.svg"} alt="logo" width={40} height={25} />
         <span className="text-blue-800 font-bold text-xl">FinanSmart</span>
       </div>
       {isSignedIn ? (
