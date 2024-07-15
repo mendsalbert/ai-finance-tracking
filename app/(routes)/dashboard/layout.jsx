70b706133dc081a8387464b3afb6dedef66654b4
mendsalbert <mendsalbert@gmail.com>
2024-07-15 17:24:05 +0000

    feat: Implement auto-redirect on Dashboard for users without budgets
    This update enhances user experience by automatically redirecting users to the budget page if they don't have any budget set up in the Dashboard layout. It checks if there are any budgets associated with a user's email address upon Dashboard component mounting and navigates the user to the budget page if none found.
    
    While no critical changes, it incorporates code style improvements for more readable and maintainable code, like consistent spacing and semicolons. A minor change to the Header component was done which doesn't bring any functional changes.
    
    No concerns or issues have been identified with these changes.

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,42 +1,41 @@
-"use client"
-import React, { useEffect } from 'react'
-import SideNav from './_components/SideNav'
-import DashboardHeader from './_components/DashboardHeader'
-import { db } from '@/utils/dbConfig'
-import { Budgets } from '@/utils/schema'
-import { useUser } from '@clerk/nextjs'
-import { eq } from 'drizzle-orm'
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useEffect } from "react";
+import SideNav from "./_components/SideNav";
+import DashboardHeader from "./_components/DashboardHeader";
+import { db } from "@/utils/dbConfig";
+import { Budgets } from "@/utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { useRouter } from "next/navigation";
 
-function DashboardLayout({children}) {
+function DashboardLayout({ children }) {
+  const { user } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    user && checkUserBudgets();
+  }, [user]);
 
-  const {user}=useUser();
-  const router=useRouter();
-  useEffect(()=>{
-    user&&checkUserBudgets();
-  },[user])
-  
-  const checkUserBudgets=async()=>{
-    const result=await db.select()
-    .from(Budgets)
-    .where(eq(Budgets.createdBy,user?.primaryEmailAddress?.emailAddress))
+  const checkUserBudgets = async () => {
+    const result = await db
+      .select()
+      .from(Budgets)
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
     console.log(result);
-    if(result?.length==0)
-    {
-      router.replace('/dashboard/budgets')
+    if (result?.length == 0) {
+      router.replace("/dashboard/budgets");
     }
-  }
+  };
   return (
     <div>
-        <div className='fixed md:w-64 hidden md:block '>
-            <SideNav/>
-        </div>
-        <div className='md:ml-64 '>
-          <DashboardHeader/>
+      <div className="fixed md:w-64 hidden md:block ">
+        <SideNav />
+      </div>
+      <div className="md:ml-64 ">
+        <DashboardHeader />
         {children}
-        </div>
-        </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default DashboardLayout
+export default DashboardLayout;
